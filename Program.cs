62685b3849ba1b3b0322441254c3f59d8e83d175
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-04-27 13:00:50 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using BeforeOurTime.Repository.Dbs.EF;
+using BeforeOurTime.Repository.Models;
+using BeforeOurTime.Repository.Models.Items;
+using BeforeOutTime.Repository.Dbs.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BeforeOurTime.Business
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { set; get; }
+        public static IServiceProvider ServiceProvider { set; get; }
+        static void Main(string[] args)
+        {
+            // Setup configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            // Setup services
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var db = new BaseContext((new DbContextOptionsBuilder<BaseContext>()).UseSqlite(connectionString).Options);
+            // Setup service provider
+            ServiceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddDbContext<BaseContext>(options => options.UseSqlite(connectionString))
+                .AddSingleton<IItemRepo<Item>>(new ItemRepo<Item>(db))
+                .BuildServiceProvider();
+
+            var setup = new Setups.Setup(Configuration, ServiceProvider);
+            setup.Install();
+
+            var itemRepo = ServiceProvider.GetService<IItemRepo<Item>>();
+            var item = itemRepo.ReadUuid(new List<Guid>() { new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5") }).First();
+
+            Console.WriteLine(item.UuidType);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Setups/Setup.cs
@@ -0,0 +1,52 @@
+﻿using BeforeOurTime.Repository.Models.Items;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Business.Setups
+{
+    public class Setup
+    {
+        public static IConfigurationRoot Configuration { set; get; }
+        public static IServiceProvider ServiceProvider { set; get; }
+
+        public Setup(IConfigurationRoot configuration, IServiceProvider serviceProvider)
+        {
+            Configuration = configuration;
+            ServiceProvider = serviceProvider;
+        }
+        public Setup Install()
+        {
+            InstallItems();
+            return this;
+        }
+        public Setup InstallItems()
+        {
+            var itemRepo = (IItemRepo<Item>) ServiceProvider.GetService(typeof(IItemRepo<Item>));
+            if (itemRepo.ReadUuid(new List<Guid>() { new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5") }).Count == 0)
+            {
+                itemRepo.Create(new List<Item>()
+                {
+                    new Item()
+                    {
+                        Type = ItemType.Generic,
+                        Uuid = new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5"),
+                        UuidType = new Guid("f08045bc-86b1-4c03-bb71-3b7f95dbd7c2"),
+                        ParentId = null,
+                        Children = new List<Item>(),
+                        Data = @"{
+                            count: 23
+                        }",
+                        Script = @"
+                            (function() {
+                                bot.log(bot.me.data.count);
+                            })();
+                        "
+                    }
+                });
+            }
+            return this;
+        }
+    }
+}
