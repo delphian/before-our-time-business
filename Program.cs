99a3d95a6f7d29d431713f88e6dea2ed4df14839
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-04-27 13:40:37 -0700

    Adding ability to script items

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 using BeforeOurTime.Repository.Models;
 using BeforeOurTime.Repository.Models.Items;
 using BeforeOutTime.Repository.Dbs.EF;
+using Jint;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -40,7 +42,12 @@ namespace BeforeOurTime.Business
             var itemRepo = ServiceProvider.GetService<IItemRepo<Item>>();
             var item = itemRepo.ReadUuid(new List<Guid>() { new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5") }).First();
 
-            Console.WriteLine(item.UuidType);
+            var engine = new Engine()
+                .SetValue("log", new Action<object>(Console.WriteLine))
+                .SetValue("me", item)
+                .SetValue("data", JObject.Parse(item.Data));
+            engine.Execute(item.Script);
+
             Console.WriteLine("Done");
             Console.ReadLine();
         }
--- Setups/Setup.cs
+++ Setups/Setup.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BeforeOurTime.Business.Setups
@@ -24,28 +25,30 @@ namespace BeforeOurTime.Business.Setups
         public Setup InstallItems()
         {
             var itemRepo = (IItemRepo<Item>) ServiceProvider.GetService(typeof(IItemRepo<Item>));
-            if (itemRepo.ReadUuid(new List<Guid>() { new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5") }).Count == 0)
+            var items = itemRepo.ReadUuid(new List<Guid>() { new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5") }).ToList();
+            if (items != null)
             {
-                itemRepo.Create(new List<Item>()
-                {
-                    new Item()
-                    {
-                        Type = ItemType.Generic,
-                        Uuid = new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5"),
-                        UuidType = new Guid("f08045bc-86b1-4c03-bb71-3b7f95dbd7c2"),
-                        ParentId = null,
-                        Children = new List<Item>(),
-                        Data = @"{
-                            count: 23
-                        }",
-                        Script = @"
-                            (function() {
-                                bot.log(bot.me.data.count);
-                            })();
-                        "
-                    }
-                });
+                itemRepo.Delete(items);
             }
+            itemRepo.Create(new List<Item>()
+            {
+                new Item()
+                {
+                    Type = ItemType.Generic,
+                    Uuid = new Guid("fe178ad7-0e33-4111-beaf-6dfcfd548bd5"),
+                    UuidType = new Guid("f08045bc-86b1-4c03-bb71-3b7f95dbd7c2"),
+                    ParentId = null,
+                    Children = new List<Item>(),
+                    Data = @"{
+                        sample: 23
+                    }",
+                    Script = @"
+                        (function() {
+                            log(parseInt(data.sample) + 1);
+                        })();
+                    "
+                }
+            });
             return this;
         }
     }
