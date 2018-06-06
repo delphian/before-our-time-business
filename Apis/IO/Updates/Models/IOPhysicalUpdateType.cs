0446406420a786ed1f145a1b862a5491d5c3f374
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-06-05 23:02:16 -0700

    Physical attributes have update message

---
+++ Apis/IO/Updates/Models/IOPhysicalUpdateType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeforeOurTime.Business.Apis.IO.Updates.Models
+{
+    /// <summary>
+    /// Type of physical activity attributes are invoking
+    /// </summary>
+    public enum IOPhysicalUpdateType : int
+    {
+        Unknown = 0,
+        Arriving = 1,
+        Departing = 2,
+        Speaking = 3,
+        Emoting = 4,
+    }
+}
