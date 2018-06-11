ffaf02a30084e0d6d6220024e61ec4f757adc0c2
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-06-11 16:15:33 -0700

    Adding base script delegate class

--- Apis/Scripts/Delegates/OnTerminalInput/OnTerminalInputDelegate.cs
+++ Apis/Scripts/Delegates/OnTerminalInput/OnTerminalInputDelegate.cs
@@ -8,29 +8,17 @@ namespace BeforeOurTime.Business.Apis.Scripts.Delegates.OnTerminalInput
     /// <summary>
     /// Structured message from a terminal
     /// </summary>
-    public class OnTerminalInputDelegate : IDelegate
+    public class OnTerminalInputDelegate : ScriptDelegate, IDelegate
     {
         /// <summary>
         /// Constructor
         /// </summary>
         public OnTerminalInputDelegate()
         {
-        }
-        public string GetFunctionName()
-        {
-            return "onTerminalInput";
-        }
-        public Type GetArgumentType()
-        {
-            return typeof(OnTerminalInputArgument);
-        }
-        public Type GetReturnType()
-        {
-            return typeof(OnTerminalInputReturn);
-        }
-        public Guid GetId()
-        {
-            return new Guid("052a078e-9814-474b-8c63-b6968b8ebaad");
+            DelegateId = new Guid("052a078e-9814-474b-8c63-b6968b8ebaad");
+            DelegateFunctionName = "onTerminalInput";
+            DelegateArgumentType = typeof(OnTerminalInputArgument);
+            DelegateReturnType = typeof(OnTerminalInputReturn);
         }
     }
 }
