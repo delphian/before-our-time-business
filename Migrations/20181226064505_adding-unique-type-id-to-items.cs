9ed286cb0b1ae2125b1315675c822fc14dc64f24
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-12-25 22:46:02 -0800

    Adding unique item type id to all items

---
+++ Migrations/20181226064505_adding-unique-type-id-to-items.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeforeOurTime.Business.Migrations
+{
+    public partial class addinguniquetypeidtoitems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TypeId",
+                table: "Items",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                table: "Items");
+        }
+    }
+}
